d937841b34d3f5f9dacfc19bbed948f1ed4d8bc7
oyewosolomon <oyewosolomon@gmail.com>
2025-03-28 18:19:40 +0100

    font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
+      },
+      fontWeight: {
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      },
       animation: {
         'fade-in': 'fadeIn 1s ease-in-out',
         'fade-in-up': 'fadeInUp 1s ease-in-out',
